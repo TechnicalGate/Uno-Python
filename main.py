afff41290d633a9da3c51dcf932b9374d544105e
Fern <mrmuffy01@gmail.com>
2025-02-12 08:38:19 +0800

    Update main.py
    make customisable part clearer

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from TurnManager import TurnManager
 
 turns = TurnManager()
 
+#===CUSTOMISATION SECTION===
 bot = Bot('Bot',turns)
 me = Human('You',turns)
 turns.add(bot)
 turns.add(me)
+#===END OF CUSTOMISATION SECTION===
+
 while True:
     turns.next_turn()
     if turns.winvar == True:
